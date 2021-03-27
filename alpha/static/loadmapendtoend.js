614388ff4eed2136d0360053c0f35fdcc6805b8c
Indu <indup@princeton.edu>
2021-03-27 11:18:01 -0500

    separate prototype and alpha version

--- alpha/static/loadmapendtoend.js
+++ alpha/static/loadmapendtoend.js
@@ -66,7 +66,13 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
           if (!graphic.attributes.layerId) { // Bandaid solution to clicking on map when no graphics
             // console.log("Clicked on graphic"); // console.log for testing purposes
             // console.log(graphic.attributes); // Attributes of graphic
-            $(".modal-title").text(graphic.attributes["name"] + " " + graphic.attributes["type"]); // Modify the modal
+
+            let titleString = graphic.attributes["type"] + " - " + graphic.attributes["name"];
+            $(".modal-title").text(titleString); // Modify the modal
+
+            let bodyString = "Details: " + graphic.attributes["building"];
+            $(".modal-body-desc").text(bodyString);
+
             $("#modalTrigger").click(); // Open the modal
           }
         }
@@ -90,9 +96,60 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
         contentType: "application/json",
         success: function(json_data){
           data_array = JSON.parse(json_data)
+          console.log(data_array);
           for (var i = 0; i < data_array.locations.location.length; i++) {
               var printer = data_array.locations.location[i];
-              addPoint(printer.geoloc.long, printer.geoloc.lat, [252, 65,3], {name: printer.name, type:"Printer"});
+              addPoint(printer.geoloc.long, printer.geoloc.lat, [252, 65,3], {name: printer.name, type:"Printer", building: printer.building.name});
+          }
+        }
+      });
+    });
+
+    // Computer Clusters
+    $("#clusters").click(function(){
+      $.ajax({
+        type: "POST",
+        url: "https://tigertools.herokuapp.com/points",
+        data: JSON.stringify({categoryid: 6}),
+        contentType: "application/json",
+        success: function(json_data){
+          data_array = JSON.parse(json_data)
+          console.log(data_array);
+          for (var i = 0; i < data_array.locations.location.length; i++) {
+              var cluster = data_array.locations.location[i];
+              var amenities = cluster.amenities.amenity
+              for (var j = 0; j < amenities.length; j++) {
+                console.log("reached")
+                if (amenities[j].name.indexOf("Macs") !== -1) {
+                  console.log("hi!");
+                  addPoint(cluster.geoloc.long, cluster.geoloc.lat, [128, 128, 128], {name: cluster.name, type:"Computer Cluster", building: cluster.building.name});
+                }
+              }
+          }
+        }
+      });
+    });
+
+    // Scanners
+    $("#scanners").click(function(){
+      $.ajax({
+        type: "POST",
+        url: "https://tigertools.herokuapp.com/points",
+        data: JSON.stringify({categoryid: 6}),
+        contentType: "application/json",
+        success: function(json_data){
+          data_array = JSON.parse(json_data)
+          console.log(data_array);
+          for (var i = 0; i < data_array.locations.location.length; i++) {
+              var scanner = data_array.locations.location[i];
+              var amenities = scanner.amenities.amenity
+              for (var j = 0; j < amenities.length; j++) {
+                console.log("reached")
+                if (amenities[j].name.indexOf("Scanners") !== -1) {
+                  console.log("hi!");
+                  addPoint(scanner.geoloc.long, scanner.geoloc.lat, [128, 128, 128], {name: scanner.name, type:"Scanner", building: scanner.building.name});
+                }
+              }
           }
         }
       });
@@ -107,9 +164,10 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
         contentType: "application/json",
         success: function(json_data){
           data_array = JSON.parse(json_data)
+          console.log(data_array);
           for (var i = 0; i < data_array.locations.location.length; i++) {
               var dhall = data_array.locations.location[i];
-              addPoint(dhall.geoloc.long, dhall.geoloc.lat, [3,65,252], {name: dhall.name, type:""});
+              addPoint(dhall.geoloc.long, dhall.geoloc.lat, [3,65,252], {name: dhall.name, type:"Dining hall", building: dhall.building.name});
           }
         }
       });
@@ -124,16 +182,31 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
         contentType: "application/json",
         success: function(json_data){
           data_array = JSON.parse(json_data)
+          console.log(data_array);
           for (var i = 0; i < data_array.locations.location.length; i++) {
               var cafe = data_array.locations.location[i];
-              addPoint(cafe.geoloc.long, cafe.geoloc.lat, [3,252,65], {name: cafe.name, type:""});
+              addPoint(cafe.geoloc.long, cafe.geoloc.lat, [3,252,65], {name: cafe.name, type:"Café", building: cafe.building.name});
           }
         }
       });
     });
 
-    // Bathrooms
-    $("#bathrooms").click(function(){
+    // Cafés
+    $("#veding").click(function(){
+      $.ajax({
+        type: "POST",
+        url: "https://tigertools.herokuapp.com/points",
+        data: JSON.stringify({categoryid: 4}),
+        contentType: "application/json",
+        success: function(json_data){
+          data_array = JSON.parse(json_data)
+          console.log(data_array);
+          for (var i = 0; i < data_array.locations.location.length; i++) {
+              var vending_machine = data_array.locations.location[i];
+              addPoint(vending_machine.geoloc.long, vending_machine.geoloc.lat, [0,0,0], {name: vending_machine.name, type:"Vending Machine", building: vending_machine.building.name});
+          }
+        }
+      });
     });
   });
 
