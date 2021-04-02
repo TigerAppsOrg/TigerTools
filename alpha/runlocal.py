a73d61307dba80ea64f9b2738d3b73c8bf410fcb
Indu <indup@princeton.edu>
2021-04-01 22:00:52 -0500

    edited work orders form, updated .py files

--- alpha/runlocal.py
+++ alpha/runlocal.py
@@ -6,11 +6,11 @@
 from flask import Flask, request
 import sys
 import os
+from handlerequests import app
 
 def main():
 	port = int(os.environ.get("PORT", 5000))
-	data = app.run(host='0.0.0.0', port=port, debug=True)
-	return data
+	app.run(host='0.0.0.0', port=port, debug=True)
 
 if __name__ == "__main__":
 	data = main()
