065df68f4a1e36cd8e631a1e5f5f45d8a209c86f
Indu <indup@princeton.edu>
2021-04-03 15:34:37 -0500

    integrated comments code

--- alpha/static/loadmapendtoend.js
+++ alpha/static/loadmapendtoend.js
@@ -1,7 +1,14 @@
-
 require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], function (esriConfig, Map, MapView, Graphic) {
   esriConfig.apiKey = "AAPKa10cbf4f4ee84d8a81f04d2002446fd8Y_3foKUUP7kErbyIPzQ_yAgYfKJhlcjIrHc-ig9_ZkQC1IaANThkbpGKv4PJlCW9";
 
+  var cafeClicks = 0;
+  var printerClicks = 0;
+  var diningClicks = 0;
+  var clusterClicks = 0;
+  var scannerClicks = 0;
+  var vendingClicks = 0;
+
+  let currentAmenityName = "";
   var map = new Map({
     // https://developers.arcgis.com/javascript/latest/api-reference/esri-Map.html
     // arcgis-navigation and arcgis-navigation-night look interesting - could do something with day/nighttime?
@@ -49,12 +56,16 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
     view.graphics.add(pointGraphic);
   }
 
-  /*
-  // Some test points
-  addPoint(-74.657, 40.346, [226,119,40], { name:"1903", type:"printer" });
-  addPoint(-74.658, 40.347, [3,65,252], { name:"McCormick", type:"library" });
-  addPoint(-74.656, 40.345, [252,3,3], { name:"Wilcox", type:"dining" });
-  */
+  function removeGraphic(amenityType) {
+    if (view.graphics.length) {
+      for (i = 0; i < view.graphics.length; i++) {
+         if (view.graphics.getItemAt(i).attributes.type == amenityType) {
+             view.graphics.remove(view.graphics.getItemAt(i));
+             i--;
+         }
+       }
+    }
+  }
 
   // Function for point clicks
   view.on("click", function(event) {
@@ -64,11 +75,45 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
         if (response.results.length) {
           const graphic = response.results[0].graphic;
           if (!graphic.attributes.layerId) { // Bandaid solution to clicking on map when no graphics
+            // console.log("Clicked on graphic"); // console.log for testing purposes
+            // console.log(graphic.attributes); // Attributes of graphic
+
             let titleString = graphic.attributes["type"] + " - " + graphic.attributes["name"];
             $(".modal-title").text(titleString); // Modify the modal
 
             let bodyString = "Details: " + graphic.attributes["building"];
             $(".modal-body-desc").text(bodyString);
+
+            currentAmenityName = graphic.attributes["type"] + " - " + graphic.attributes["name"];
+            $.ajax({
+              type: "POST",
+              url: "https://tigertools-test.herokuapp.com/displaycomments",
+              data: JSON.stringify({amenityName: titleString}),
+              contentType: "application/json",
+              success: function(response){
+                $("#nav-info").html(response);
+              }
+            });
+
+            /*$.ajax({
+              type: "POST",
+              url: "http://0.0.0.0:5000/getcomments",
+              data: JSON.stringify({AmenityName: $(".modal-body-desc").text()}),
+              contentType: "application/json",
+              success: function(comments){
+                $.ajax({
+                  type: "POST",
+                  url: "http://0.0.0.0:5000/displaycomments",
+                  data: comments,
+                  contentType: "application/json",
+                  success: function(response){
+                    //console.log(comments.list_of_data[0][1]);
+                    $("#nav-info").html(response);
+                  }
+                });
+                console.log(comments.list_of_data[0][1]);
+              }
+            });*/
 
             $("#modalTrigger").click(); // Open the modal
           }
@@ -78,33 +123,52 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
 
   $(document).ready(function(){
 
-    // Validate required fields in work order form when user clicks submit
-    $("#open-confirmation").click(function(event){
-      var form = $("#workorder-form");
-      if (form[0].checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
+    // submit comment
+    $("#submitcomment").click(function(){
+      var comment = $.trim($("#message-text").val());
+      //var comment = $("#comment").val();
+      //console.log(comment);
+      if(comment != ""){
+                // Show alert dialog if value is not blank
+                //alert("Please enter a comment");
+                $.ajax({
+                  type: "POST",
+                  url: "https://tigertools-test.herokuapp.com/comment",
+                  data: JSON.stringify({amenityName: currentAmenityName, textComment: comment}),
+                  contentType: "application/json",
+                  success: function(comment){
+                    $("#message-text").val("");
+                  }
+                });
+      } else {
+        alert("Please enter a comment")
       }
-          
-      form.addClass('was-validated');
-    });
-
-    // Close the confirmation modal when clicking submit button
-    $("#confirm-btn").click(function(){
-      $("#confirm-close").click();
     });
 
     // Reset/remove all graphics
     $("#reset").click(function(){
+      cafeClicks++;
+      printerClicks++;
+      diningClicks++;
+      clusterClicks++;
+      scannerClicks++;
+      vendingClicks++;
       view.graphics.removeAll();
     });
 
-    // Printers
-    // PRINTER NEEDS TO BE REFINED: CURRENTLY SHOWING SCANNERS AND OTHER STUFF TOO, broken for some reason too
+
+    //$(".btn").click(function(e) {
+    //e.preventDefault();
+    //$(this).addClass('active');
+    //});
+
+    // Fixed Printers
     $("#printers").click(function(){
+      if (printerClicks % 2 == 0) {
       $.ajax({
         type: "POST",
-        url: "/points",
+        url: "https://tigertools-test.herokuapp.com/points",
+        //url: "http://0.0.0.0:5000/points",
         data: JSON.stringify({categoryid: 6}),
         contentType: "application/json",
         success: function(json_data){
@@ -112,17 +176,31 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
           console.log(data_array);
           for (var i = 0; i < data_array.locations.location.length; i++) {
               var printer = data_array.locations.location[i];
-              addPoint(printer.geoloc.long, printer.geoloc.lat, [252, 65,3], {name: printer.name, type:"Printer", building: printer.building.name});
+              var amenities = printer.amenities.amenity
+              for (var j = 0; j < amenities.length; j++) {
+                //console.log("reached")
+                if (amenities[j].name.indexOf("Printers") !== -1) {
+                  //console.log("hi!");
+                  addPoint(printer.geoloc.long, printer.geoloc.lat, [252, 65, 3], {name: printer.name, type:"Printer", building: printer.building.name});
+                }
+              }
           }
         }
       });
+      printerClicks++;
+    } else {
+      printerClicks++;
+      removeGraphic("Printer")
+    }
     });
 
     // Computer Clusters
     $("#clusters").click(function(){
+      if (clusterClicks % 2 == 0) {
       $.ajax({
         type: "POST",
-        url: "/points",
+        url: "https://tigertools-test.herokuapp.com/points",
+        //url: "http://0.0.0.0:5000/points",
         data: JSON.stringify({categoryid: 6}),
         contentType: "application/json",
         success: function(json_data){
@@ -132,20 +210,29 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
               var cluster = data_array.locations.location[i];
               var amenities = cluster.amenities.amenity
               for (var j = 0; j < amenities.length; j++) {
+                //console.log("reached")
                 if (amenities[j].name.indexOf("Macs") !== -1) {
+                  //console.log("hi!");
                   addPoint(cluster.geoloc.long, cluster.geoloc.lat, [128, 128, 128], {name: cluster.name, type:"Computer Cluster", building: cluster.building.name});
                 }
               }
           }
         }
       });
+      clusterClicks++;
+    } else {
+      clusterClicks++;
+      removeGraphic("Computer Cluster")
+    }
     });
 
     // Scanners
     $("#scanners").click(function(){
+      if (scannerClicks % 2 == 0) {
       $.ajax({
         type: "POST",
-        url: "/points",
+        url: "https://tigertools-test.herokuapp.com/points",
+        //url: "http://0.0.0.0:5000/points",
         data: JSON.stringify({categoryid: 6}),
         contentType: "application/json",
         success: function(json_data){
@@ -155,22 +242,29 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
               var scanner = data_array.locations.location[i];
               var amenities = scanner.amenities.amenity
               for (var j = 0; j < amenities.length; j++) {
-                console.log("reached")
+                //console.log("reached")
                 if (amenities[j].name.indexOf("Scanners") !== -1) {
-                  console.log("hi!");
-                  addPoint(scanner.geoloc.long, scanner.geoloc.lat, [255, 193, 7], {name: scanner.name, type:"Scanner", building: scanner.building.name});
+                  //console.log("hi!");
+                  addPoint(scanner.geoloc.long, scanner.geoloc.lat, [238, 210, 2], {name: scanner.name, type:"Scanner", building: scanner.building.name});
                 }
               }
           }
         }
       });
+      scannerClicks++;
+    } else {
+      scannerClicks++;
+      removeGraphic("Scanner")
+    }
     });
 
     // Dining Halls
     $("#dhalls").click(function(){
+      if (diningClicks % 2 == 0) {
       $.ajax({
         type: "POST",
-        url: "/points",
+        url: "https://tigertools-test.herokuapp.com/points",
+        //url: "http://0.0.0.0:5000/points",
         data: JSON.stringify({categoryid: 2}),
         contentType: "application/json",
         success: function(json_data){
@@ -182,13 +276,20 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
           }
         }
       });
+      diningClicks++;
+    } else {
+      diningClicks++;
+      removeGraphic("Dining hall")
+    }
     });
 
     // Cafés
     $("#cafes").click(function(){
+      if (cafeClicks % 2 == 0) {
       $.ajax({
         type: "POST",
-        url: "/points",
+        url: "https://tigertools-test.herokuapp.com/points",
+        //url: "http://0.0.0.0:5000/points",
         data: JSON.stringify({categoryid: 3}),
         contentType: "application/json",
         success: function(json_data){
@@ -200,13 +301,20 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
           }
         }
       });
+      cafeClicks++;
+    } else {
+      cafeClicks++;
+      removeGraphic("Café")
+    }
     });
 
     // Vending Machines
     $("#vending").click(function(){
+      if (vendingClicks % 2 == 0) {
       $.ajax({
         type: "POST",
-        url: "/points",
+        url: "https://tigertools-test.herokuapp.com/points",
+        //url: "http://0.0.0.0:5000/points",
         data: JSON.stringify({categoryid: 4}),
         contentType: "application/json",
         success: function(json_data){
@@ -218,6 +326,11 @@ require(["esri/config","esri/Map", "esri/views/MapView", "esri/Graphic"], functi
           }
         }
       });
+      vendingClicks++;
+    } else {
+      vendingClicks++;
+      removeGraphic("Vending Machine")
+    }
     });
   });
 
