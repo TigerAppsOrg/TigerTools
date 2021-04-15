0b2888e31b52f1f25444328153bbd5cb7bfd2909
Indu <indup@princeton.edu>
2021-04-15 06:43:40 -0500

    fixed sql

--- beta/handlerequests.py
+++ beta/handlerequests.py
@@ -88,19 +88,19 @@ def get_data():
 		if amenity_type == "printers":
 			# get records
 			stmt = 'SELECT * FROM id6 WHERE printers<>%s;'
-			dbcursor.execute(stmt,("None",))
+			dbcursor.execute(stmt,('None',))
 			data = dbcursor.fetchall()
 			data_json = _tuples_to_json(cols, data)
 
 		elif amenity_type == "scanners":
 			stmt = 'SELECT * FROM id6 WHERE scanners<>%s;'
-			dbcursor.execute(stmt,("None",))
+			dbcursor.execute(stmt,('None',))
 			data = dbcursor.fetchall()
 			data_json = _tuples_to_json(cols, data)
 
 		elif amenity_type == "macs":
 			stmt = 'SELECT * FROM id6 WHERE macs<>%s;'
-			dbcursor.execute(stmt,("None",))
+			dbcursor.execute(stmt,('None',))
 			data = dbcursor.fetchall()
 			print(data)
 			data_json = _tuples_to_json(cols, data)
