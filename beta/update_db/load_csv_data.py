7631c08904b1cc61f0b976606b637f93c8fb4d29
Indu <indup@princeton.edu>
2021-04-15 21:52:36 -0500

    fixed sql

--- beta/update_db/load_csv_data.py
+++ beta/update_db/load_csv_data.py
@@ -48,6 +48,7 @@ def load_water(dbcursor):
 def load_buildings(dbcursor):
 	dbcursor.execute('DROP TABLE IF EXISTS buildings')
 	dbcursor.execute('CREATE TABLE buildings (locationcode VARCHAR(6), name VARCHAR(110), lat VARCHAR(20), long VARCHAR(20))')
+	dbcursor.execute('UPDATE buildings SET locationcode=CONCAT(locationcode,\'%\');')
 	csv_file_name = '/Users/indup/Documents/TigerTools/beta/update_db/buildings.csv'
 	sql = "COPY buildings FROM STDIN CSV HEADER"
 	dbcursor.copy_expert(sql, open(csv_file_name, "r"))
