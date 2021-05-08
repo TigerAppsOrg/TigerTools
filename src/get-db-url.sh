6e6513e7fb448e3c37fa8c2cef8abccb016b4743
ind1010 <indup@princeton.edu>
2021-05-08 13:58:58 -0500

    cleaned up shell scripts

--- src/get-db-url.sh
+++ src/get-db-url.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# might switch to DATABASE_URL=$(heroku config:get DATABASE_URL -a your-app) your_process
-
-# heroku config:get DATABASE_URL -a tigertools
+# For local testing. Sets an environment variable containing our database URL for Heroku.
 
 export DATABASE_URL=$(heroku config:get DATABASE_URL -a tigertools)
